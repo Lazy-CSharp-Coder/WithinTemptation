99afd3cf5a8b8009e5edab16eba7403f7bcb6499
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-11-05 08:26:16 +0100

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playlist = [ new Audio("MP3/01 - Why Not Me.mp3"),    
+const playlist = [ new Audio("Album/tracks/01 - Why Not Me.mp3"),    
                 //    new Audio("MP3/02 - Shot In The Dark.mp3"),
                 //    new Audio("Album/tracks/03 - In The Middle Of The Night.mp3"),    
                 //   new Audio("Album/tracks/04 - Faster.mp3"),
